8c0c3cedfc0a240034b0bd5d7aaf4c274d9a801a
my.arafat <my.arafat@blmanagedservices,com>
2022-01-05 19:16:14 +0600

    Load file into c# by powershell

--- CMDPlay/PowerShellForm.Designer.cs
+++ CMDPlay/PowerShellForm.Designer.cs
@@ -29,7 +29,57 @@ namespace CMDPlay
         /// </summary>
         private void InitializeComponent()
         {
+            this.logTxtBox = new System.Windows.Forms.TextBox();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // logTxtBox
+            // 
+            this.logTxtBox.Location = new System.Drawing.Point(11, 45);
+            this.logTxtBox.Name = "logTxtBox";
+            this.logTxtBox.Size = new System.Drawing.Size(257, 23);
+            this.logTxtBox.TabIndex = 8;
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(289, 44);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoadBtn.TabIndex = 6;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Log File";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(11, 161);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(781, 275);
+            this.dataGridView.TabIndex = 9;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // PowerShellForm
             // 
@@ -37,12 +87,24 @@ namespace CMDPlay
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(804, 451);
             this.ControlBox = false;
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.logTxtBox);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.label3);
             this.Name = "PowerShellForm";
             this.Text = "PowerShellForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox logTxtBox;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- CMDPlay/PowerShellForm.cs
+++ CMDPlay/PowerShellForm.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,44 @@ namespace CMDPlay
         public PowerShellForm()
         {
             InitializeComponent();
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            //var path = logTxtBox.Text;
+            //if (string.IsNullOrWhiteSpace(path))
+            //{
+            //    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //    {
+            //        path = openFileDialog1.FileName;
+            //        logTxtBox.Text = path;
+            //    }
+            //}
+            LoadLog();
+        }
+        public void LoadLog()
+        {
+            //string path = logTxtBox.Text;
+            string path = "C:\\Users\\my.arafat\\u_ex211222_x.log";
+            string cmd = "Import-Csv -Delimiter ' ' -Header ((Get-Content "+ path+" -ReadCount 4 -TotalCount 4)[3].split(' ') | Where-Object { $_ -ne '#Fields:' }) -Path "+path+" | Where-Object { $_.date -notlike '#*' }";
+            Collection<PSObject> obj = ExecutePowershell<Collection<PSObject>>(cmd);
+            ArrayList objects = new ArrayList();
+            objects.AddRange(obj);
+            dataGridView.DataSource = objects;
+        }
+        public T ExecutePowershell<T>(string command) 
+        {
+
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                PowerShellInstance.AddScript(command);
+                if(typeof(T) == typeof(Collection<PSObject>))
+                {
+                    Collection<PSObject>  result = PowerShellInstance.Invoke();
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+            return default;
         }
     }
 }
